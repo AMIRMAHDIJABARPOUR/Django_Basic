c0f5dd17b9290d5cfac7913257070dc966036dfc
AMIR <ajaba2021@gmail.com>
2025-05-24 02:25:43 +0330

    first practice in season 2

--- practice1.py
+++ practice1.py
@@ -1,1 +1,37 @@
-print("hello world ")
+# practice 1
+import random
+
+num = random.randint(0, 100)
+
+
+def odd_or_even(num):
+    if num % 2 == 0:
+        return "odd"
+    else:
+        return "even"
+
+
+# # practice 2
+number = int(input("Enter a number : "))
+
+
+def result(number):
+    Total = 0
+    for num in number:
+        Total += num
+    return Total
+
+
+# practice 3
+text = input("enter your text :")
+numbers = 0
+character = 0
+
+for word in text:
+    if word == " ":
+        continue
+    elif word in "0123456789":
+        numbers += 1
+    else:
+        character += 1
+print(f"character : {character}  numbers : {numbers}")
